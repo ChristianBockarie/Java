fae4332ee5a8b1d08bd758dc0960f143ec11abbb
ChristianBockarie <avanchristianbock.33sl@gmail.com>
2019-03-28 21:56:25 -0400

    new

---
+++ src/com/Class13/Task.java
@@ -0,0 +1,14 @@
+package com.Class13;
+
+public class Task {
+	public static void main(String[] args) {
+
+		// Create a String and print it in reverse order (Sunday---->yadnuS)
+
+		String day = "Sunday";
+		for (int i=day.length()-1; i>=0; i--) {
+		System.out.print((day.charAt(i)));
+	}
+
+}
+}
